4aa5d6edc56c2851d530ace108357d54491a7863
AnhQuocs <anhquocydkl@gmail.com>
2025-12-23 10:30:27 +0700

    Restructure project: move source into SRC folder

---
+++ SRC/app/src/main/java/com/example/hotelbooking/features/onboarding/ui/OnboardingScreen.kt
@@ -0,0 +1,17 @@
+package com.example.hotelbooking.features.onboarding.ui
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+data class OnboardingData(
+    val img: Int,
+    val title: String,
+    val description: String
+)
+
+@Composable
+fun OnboardingScreen() {
+    val items = listOf(
+//        OnboardingData()
+    )
+}
---
+++ SRC/app/src/main/java/com/example/hotelbooking/features/onboarding/ui/PagerIndicator.kt
@@ -0,0 +1,51 @@
+package com.example.hotelbooking.features.onboarding.ui
+
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import com.example.hotelbooking.ui.theme.PrimaryBlue
+
+@Composable
+fun PagerIndicator(
+    size: Int,
+    currentPage: Int,
+    modifier: Modifier = Modifier
+) {
+    Row(
+        horizontalArrangement = Arrangement.SpaceBetween,
+        modifier = Modifier.padding(top = 60.dp)
+    ) {
+        repeat(size) {
+            Indicator(isSelected = it == currentPage)
+            Spacer(modifier = Modifier.width(4.dp))
+        }
+    }
+}
+
+@Composable
+fun Indicator(isSelected: Boolean) {
+    val width = animateDpAsState(targetValue = if (isSelected) 25.dp else 12.dp, label = "")
+
+    Box(
+        modifier = Modifier
+            .padding(1.dp)
+            .height(10.dp)
+            .width(width.value)
+            .clip(CircleShape)
+            .background(
+                if(isSelected) PrimaryBlue else Color.White
+            )
+    )
+}
